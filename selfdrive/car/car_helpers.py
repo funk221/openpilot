44cc304afc8301cfeb479d0749041c8d33f3a055
funk221 <xiaohui.liu@grace.com>
2025-07-02 20:17:06 -0400

    force to OLD_CAR

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -112,58 +112,58 @@ def fingerprint(logcan, sendcan):
   Params().put("CarVin", vin)
 
   finger = gen_empty_fingerprint()
-  candidate_cars = {i: all_legacy_fingerprint_cars() for i in [0, 1]}  # attempt fingerprint on both bus 0 and 1
-  frame = 0
-  frame_fingerprint = 10  # 0.1s
-  car_fingerprint = None
-  done = False
+#  candidate_cars = {i: all_legacy_fingerprint_cars() for i in [0, 1]}  # attempt fingerprint on both bus 0 and 1
+#  frame = 0
+#  frame_fingerprint = 10  # 0.1s
+#  car_fingerprint = None
+#  done = False
 
-  while not done:
-    a = get_one_can(logcan)
+#  while not done:
+#    a = get_one_can(logcan)
 
-    for can in a.can:
+#    for can in a.can:
       # The fingerprint dict is generated for all buses, this way the car interface
       # can use it to detect a (valid) multipanda setup and initialize accordingly
-      if can.src < 128:
-        if can.src not in finger:
-          finger[can.src] = {}
-        finger[can.src][can.address] = len(can.dat)
+#      if can.src < 128:
+#        if can.src not in finger:
+#          finger[can.src] = {}
+#        finger[can.src][can.address] = len(can.dat)
 
-      for b in candidate_cars:
+#      for b in candidate_cars:
         # Ignore extended messages and VIN query response.
-        if can.src == b and can.address < 0x800 and can.address not in (0x7df, 0x7e0, 0x7e8):
-          candidate_cars[b] = eliminate_incompatible_cars(can, candidate_cars[b])
+#        if can.src == b and can.address < 0x800 and can.address not in (0x7df, 0x7e0, 0x7e8):
+#          candidate_cars[b] = eliminate_incompatible_cars(can, candidate_cars[b])
 
     # if we only have one car choice and the time since we got our first
     # message has elapsed, exit
-    for b in candidate_cars:
-      if len(candidate_cars[b]) == 1 and frame > frame_fingerprint:
+#    for b in candidate_cars:
+#      if len(candidate_cars[b]) == 1 and frame > frame_fingerprint:
         # fingerprint done
-        car_fingerprint = candidate_cars[b][0]
+#        car_fingerprint = candidate_cars[b][0]
 
     # bail if no cars left or we've been waiting for more than 2s
-    failed = (all(len(cc) == 0 for cc in candidate_cars.values()) and frame > frame_fingerprint) or frame > 200
-    succeeded = car_fingerprint is not None
-    done = failed or succeeded
+#    failed = (all(len(cc) == 0 for cc in candidate_cars.values()) and frame > frame_fingerprint) or frame > 200
+#    succeeded = car_fingerprint is not None
+#    done = failed or succeeded
 
-    frame += 1
+#    frame += 1
 
-  exact_match = True
-  source = car.CarParams.FingerprintSource.can
+#  exact_match = True
+#  source = car.CarParams.FingerprintSource.can
 
   # If FW query returns exactly 1 candidate, use it
-  if len(fw_candidates) == 1:
-    car_fingerprint = list(fw_candidates)[0]
-    source = car.CarParams.FingerprintSource.fw
-    exact_match = exact_fw_match
+#  if len(fw_candidates) == 1:
+#    car_fingerprint = list(fw_candidates)[0]
+#    source = car.CarParams.FingerprintSource.fw
+#    exact_match = exact_fw_match
 
-  if fixed_fingerprint:
-    car_fingerprint = fixed_fingerprint
-    source = car.CarParams.FingerprintSource.fixed
+#  if fixed_fingerprint:
+#    car_fingerprint = fixed_fingerprint
+#    source = car.CarParams.FingerprintSource.fixed
 
-  cloudlog.event("fingerprinted", car_fingerprint=car_fingerprint,
-                 source=source, fuzzy=not exact_match, fw_count=len(car_fw))
-  return car_fingerprint, finger, vin, car_fw, source, exact_match
+#  cloudlog.event("fingerprinted", car_fingerprint=car_fingerprint,
+#                 source=source, fuzzy=not exact_match, fw_count=len(car_fw))
+#  return car_fingerprint, finger, vin, car_fw, source, exact_match
 
 
 def get_car(logcan, sendcan):
